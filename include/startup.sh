d7ff573e694588ae131786f8a4a4fac7ee04fecf
Shunmo17 <mochizushun@keio.jp>
2020-04-02 19:00:06 +0900

    feat: merge launch file

--- include/startup.sh
+++ include/startup.sh
@@ -11,21 +11,4 @@ if [ $CAM_NUMBER = "unspecified" ]; then
         echo "Test Mode was specifed."
     fi
 fi
-if [ $CAM_NUMBER = 0 ]; then
-    roslaunch detectron2_ros env_cam00.launch
-fi
-if [ $CAM_NUMBER = 1 ]; then
-    roslaunch detectron2_ros env_cam01.launch
-fi
-if [ $CAM_NUMBER = 2 ]; then
-    roslaunch detectron2_ros env_cam02.launch
-fi
-if [ $CAM_NUMBER = 3 ]; then
-    roslaunch detectron2_ros env_cam03.launch
-fi
-if [ $CAM_NUMBER = 4 ]; then
-    roslaunch detectron2_ros env_cam04.launch
-fi
-if [ $CAM_NUMBER = 5 ]; then
-    roslaunch detectron2_ros env_cam05.launch
-fi
+roslaunch detectron2_ros detectron2_ros.launch camera_index:= $CAM_NUMBER
