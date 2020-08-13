ea6907fafdaa9a33ab4e7990b27830f3f7194651
Shunmo17 <mochizushun@keio.jp>
2020-08-14 01:07:39 +0900

    FIX: change torchvisio version

--- include/install_detectron2.sh
+++ include/install_detectron2.sh
@@ -12,7 +12,7 @@ export WORKON_HOME=~/.virtualenvs && \
 echo '. /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc && \
 source ~/.bashrc && \
 mkvirtualenv --python=python3 detectron2_ros && \
-pip install -U torch==1.5.0+cu101 torchvision==0.5+cu100 -f https://download.pytorch.org/whl/torch_stable.html && \
+pip install -U torch==1.5.0+cu101 torchvision==0.6.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html && \
 pip install cython pyyaml==5.1 && \
 pip install -U 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI' && \
 pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu100/index.html && \
